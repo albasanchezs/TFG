7175baee15eb53c38996e51e86cf4e7b609754e4
100406577 <100406577@alumnos.uc3m.es>
2023-03-23 18:23:02 +0100

    v3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
      lis_estado = []
      dic={}
      df_prueba = pd.DataFrame()
-     ubicacion= "C:\\Users\\asanchezsanc\\Desktop\\personal\\Proyectonuevo\\des_pdfs\\" #intentar que esta carpeta este dentro del proyecto
+     ubicacion= "C:\\Users\\asanchezsanc\\Desktop\\personal\\Proyectonuevo\\des_pdfs\\"
     # ubicacion = '.\\pdf_output\\'
      #identificadores = datos_generales.creacion_tablas(configuracion['principal']['url'],df_prueba)
      #datos_web.datos_basicos(configuracion['basico']['url'], identificadores,df_prueba)
@@ -37,24 +37,12 @@ if __name__ == '__main__':
      #datos_web.datos_metodologia(configuracion['metodologia']['url'], identificadores, df_prueba)
      #datos_web.datos_sistemas(configuracion['sistemaforma']['url'], identificadores, df_prueba)
      #descarga_web.des_tabla(configuracion['pdf']['url'], identificadores, ubicacion, df_prueba)
-     identificadores = ['25000482019030501', '25001552019102501', '25000452019040301', '25000482019030501']
-     descarga_web.des_text(configuracion['pdf']['url'], identificadores, ubicacion, df_prueba)
-     print(df_prueba['Texto'].iloc[0])
 
-     """
-     iden='25029222013071801'
-     lista=[]
-     dic={}
-     df_inter=pd.DataFrame()
-     df = read_pdf(ubicacion + str(iden) + ".pdf", pages="all", multiple_tables=True, encoding='latin-1')
-     for u in range(len(df)):
-          lista.append(df[u].to_numpy().transpose().tolist())
-     print(lista)
-     dic[iden]=lista
-     df_prueba['Tabla']=dic
-     #df_arr=[df_prueba.values for df_prueba in df]
-     print(df_prueba)
-     """
+     #PRUEBAS
+    # identificadores = ['25000482019030501', '25001552019102501', '25000452019040301', '25000482019030501']
+    # descarga_web.des_text(configuracion['pdf']['url'], identificadores, ubicacion, df_prueba)
+    # print(df_prueba['Texto'].iloc[0])
+
      """
      #Guardado y leido en parquet
      df_prueba.to_parquet('data.parquet')
